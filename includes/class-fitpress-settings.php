d17e88a5c24d6f32e088c5a25862e6a601da245f
Bradford Knowlton <brad@ucanbonline.net>
2016-02-01 22:47:40 -0700

    changed friend settings to FitPress Settings

--- includes/class-fitpress-settings.php
+++ includes/class-fitpress-settings.php
@@ -37,7 +37,7 @@ if(!class_exists('FitPress_Settings'))
             // add your settings section
             add_settings_section(
                 'fitpress_settings-section', 
-                __( 'Friend Settings', 'fitpress' ), 
+                __( 'FitPress Settings', 'fitpress' ), 
                 array(&$this, 'settings_section_fitpress_settings'), 
                 'fitpress_settings'
             );
