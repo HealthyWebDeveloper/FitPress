ce9ec83fac1c6d74aa0eee7132896c3c49186510
Bradford Knowlton <brad@ucanbonline.net>
2016-02-03 19:15:10 -0700

    changed branch label in header

--- fitpress.php
+++ fitpress.php
@@ -11,7 +11,7 @@ Text Domain: fitpress
 Domain Path: /languages
 
 GitHub Plugin URI: https://github.com/HealthyWebDeveloper/FitPress
-GitHub Branch: master
+GitHub Branch: OAuth-2.0
 
 Requires PHP: 5.3.0
 Requires WP: 4.4
@@ -46,7 +46,8 @@ define( 'FITPRESS_PLUGIN_DIR', dirname(__FILE__).'/' );
 
 /** Requiring required class files */
 
-require_once( plugin_dir_path( __FILE__ ) . 'includes/class-fitbit-api.php');
+// require_once( plugin_dir_path( __FILE__ ) . 'includes/class-fitbit-api.php');
+require_once( plugin_dir_path( __FILE__ ) . 'vendor/autoload.php');
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-fitpress-settings.php');
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-fitpress-buddypress.php');
 require_once( plugin_dir_path( __FILE__ ) . 'includes/class-fitpress-shortcode.php');
@@ -56,12 +57,14 @@ require_once( plugin_dir_path( __FILE__ ) . 'includes/class-fitpress-shortcode.p
 
 
 $consumer_key = get_site_option( 'fitpress_consumer_key', false );
+$client_id = get_site_option( 'fitpress_client_id', false );
 $consumer_secret = get_site_option( 'fitpress_consumer_secret', false );
 // $fitbit_token = get_site_option( 'fitpress_fitbit_token', false );
 // $fitbit_secret = get_site_option( 'fitpress_fitbit_secret', false );
 $tab_name = get_site_option( 'fitpress_tab_name', false );
 
 define( 'FITPRESS_CONSUMER_KEY', $consumer_key );
+define( 'FITPRESS_CLIENT_ID', $client_id );
 define( 'FITPRESS_CONSUMER_SECRET', $consumer_secret );
 // define( 'FITPRESS_FITBIT_TOKEN', $fitbit_token );
 // define( 'FITPRESS_FITBIT_SECRET', $fitbit_secret );
@@ -71,7 +74,78 @@ global $fitbit_php;
 
 if( '' != FITPRESS_CONSUMER_KEY && '' != FITPRESS_CONSUMER_SECRET ){
 
-    $fitbit_php = new FitBitPHP( FITPRESS_CONSUMER_KEY, FITPRESS_CONSUMER_SECRET, 0, null, 'json' );
+    // $fitbit_php = new FitBitPHP( FITPRESS_CONSUMER_KEY, FITPRESS_CONSUMER_SECRET, 0, null, 'json' );
+
+    $provider = new djchen\OAuth2\Client\Provider\Fitbit([
+        'clientId'          => FITPRESS_CLIENT_ID,
+        'clientSecret'      => FITPRESS_CONSUMER_SECRET,
+        'redirectUri'       => get_permalink(),
+    ]);
+
+    // start the session
+    session_start();
+
+    // If we don't have an authorization code then get one
+    if (!isset($_GET['code'])) {
+
+        // Fetch the authorization URL from the provider; this returns the
+        // urlAuthorize option and generates and applies any necessary parameters
+        // (e.g. state).
+        $authorizationUrl = $provider->getAuthorizationUrl();
+
+        // Get the state generated for you and store it to the session.
+        $_SESSION['oauth2state'] = $provider->getState();
+
+        // Redirect the user to the authorization URL.
+        header('Location: ' . $authorizationUrl);
+        exit;
+
+    // Check given state against previously stored one to mitigate CSRF attack
+    } elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+        unset($_SESSION['oauth2state']);
+        exit('Invalid state');
+
+    } else {
+
+        try {
+
+            // Try to get an access token using the authorization code grant.
+            $accessToken = $provider->getAccessToken('authorization_code', [
+                'code' => $_GET['code']
+            ]);
+
+            // We have an access token, which we may use in authenticated
+            // requests against the service provider's API.
+            echo $accessToken->getToken() . "\n";
+            echo $accessToken->getRefreshToken() . "\n";
+            echo $accessToken->getExpires() . "\n";
+            echo ($accessToken->hasExpired() ? 'expired' : 'not expired') . "\n";
+
+            // Using the access token, we may look up details about the
+            // resource owner.
+            $resourceOwner = $provider->getResourceOwner($accessToken);
+
+            var_export($resourceOwner->toArray());
+
+            // The provider provides a way to get an authenticated API request for
+            // the service, using the access token; it returns an object conforming
+            // to Psr\Http\Message\RequestInterface.
+            $request = $provider->getAuthenticatedRequest(
+                'GET',
+                'https://api.fitbit.com/1/user/-/profile.json',
+                $accessToken
+            );
+            // Make the authenticated API request and get the response.
+            //$response = $provider->getResponse($request);
+
+        } catch (\League\OAuth2\Client\Provider\Exception\IdentityProviderException $e) {
+
+            // Failed to get the access token or user details.
+            exit($e->getMessage());
+
+        }
+
+    }
 
 }
 
